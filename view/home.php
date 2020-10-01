81eb3c9e741fd76e0a5f6f50d377cd8204899314
Adam McGurk <eldermcgurk@gmail.com>
2020-10-01 16:08:14 -0500

    Almost done with the front page

--- view/home.php
+++ view/home.php
@@ -4,6 +4,7 @@
 		
 		<h1 class="big">Shine PHP</h1>
 		<p>Finally...a PHP microframework that stays out of your way!</p>
+		<div id="composerStatement" class="code" style="margin-top: 0px;">composer require adammcgurk/shine-php</div>
 		<div class="flex">
 			<a href="/tutorials/" title="Get started with Shine PHP now!" class="button blue">Get Started</a>
 			<a href="/documentation/" title="Read the docs" class="button green">Read the docs</a>
@@ -16,12 +17,26 @@
 		<h1>Secure by default</h1>
 
 		<ul>
-			<li>Uses only PDO for connecting to the database…no mysqli anywhere</li>
+			<li>Uses only PDO for connecting to the database…no mysqli anywhere.</li>
 			<li>We only implement cURL securely (no certificate disabling!)</li>
-			<li>The data validation scripts are built with an "allowlist" mindset, instead of a "denylist", thereby making applications even more secure.</li>
+			<li>The data validation scripts are built with an "allowlist" mindset, instead of a "denylist", making applications even more secure.</li>
 		</ul>
 
 		<a href="/documentation/" title="Read the docs" class="button blue long">Read the docs</a>
+
+	</section>
+
+	<section class="landing">
+		
+		<h1>Easy to implement</h1>
+
+		<ul>
+			<li>No setup required...just install the composer package, and you can start coding right away!</li>
+			<li>Pick and choose the classes and libraries you want to use or include, it's that easy!</li>
+			<li>Almost <i>all</i> of the methods can be used in two lines of code!</li>
+		</ul>
+
+		<a href="/tutorials/" title="Read the tutorials" class="button green long">Read the docs</a>
 
 	</section>
 
--- view/includes/footer.php
+++ view/includes/footer.php
@@ -1,3 +1,8 @@
+<footer id="bottomFooter">
+	
+	<span>&copy;</span><a href="https://adammcgurk.com/" title="Adam McGurk" target="_blank" rel="noopener"> Adam McGurk </a><span><?=\date('Y')?></span>
+
+</footer>
 
 </body>
 </html>
